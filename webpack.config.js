44210e220e8cc50a5697db8d1cf35029a652b9b4
Maksym Pokulytyi <mlightpro@gmail.com>
2023-03-09 15:18:56 +0200

    webpack configuration test

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        // filename: "./dist/main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.png/,
+                type: "asset/resource",
+                test: /\.svg/,
+                type: "asset/inline",
+                dependency: { not: ["url"] },
+                use: [
+                    {
+                        loader: "url-loader",
+                        options: {
+                            limit: 8192,
+                        },
+                    },
+                ],
+                type: "javascript/auto",
+            },
+            {
+                test: /\.(scss|css)$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "style-loader",
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css",
+        }),
+    ],
+};
